fb5daae2278d9737f98e7b000ddd7b4cd9a6add9
raiprabhat91 <raiprabhat91@gmail.com>
2020-06-06 17:26:05 +0530

    Array DI

---
+++ Array_DI/src/beans/Car.java
@@ -0,0 +1,26 @@
+package beans;
+
+public class Car {
+	private String[] carnames;
+	private Engine[] engine;
+	public String[] getCarnames() {
+		return carnames;
+	}
+	public void setCarnames(String[] carnames) {
+		this.carnames = carnames;
+	}
+	public Engine[] getEngine() {
+		return engine;
+	}
+	public void setEngine(Engine[] engine) {
+		this.engine = engine;
+	}
+	public void printCarData(){
+		for(String car:carnames){
+			System.out.println(car);
+		}
+		for(Engine e:engine){
+			System.out.println(e.getModelyear());
+		}
+	}
+}
---
+++ Array_DI/src/beans/Engine.java
@@ -0,0 +1,14 @@
+package beans;
+
+public class Engine {
+	private String modelyear;
+
+	public String getModelyear() {
+		return modelyear;
+	}
+
+	public void setModelyear(String modelyear) {
+		this.modelyear = modelyear;
+	}
+	
+}
