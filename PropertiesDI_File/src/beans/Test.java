56b5cb3efd994aca080e929c4310621a1db3c92d
raiprabhat91 <raiprabhat91@gmail.com>
2020-06-09 12:52:32 +0530

    adding module PropertiesDI and PropertiesDI_File

---
+++ PropertiesDI_File/src/beans/Test.java
@@ -0,0 +1,23 @@
+package beans;
+
+import java.util.Properties;
+import java.util.Set;
+
+public class Test {
+	private Properties driver;
+
+	public Properties getDriver() {
+		return driver;
+	}
+
+	public void setDriver(Properties driver) {
+		this.driver = driver;
+	}
+	
+	public void printData(){
+		Set keys = driver.keySet();
+		for(Object key: keys){
+			System.out.println(key + " : "+driver.getProperty(key.toString()));
+		}
+	}
+}
